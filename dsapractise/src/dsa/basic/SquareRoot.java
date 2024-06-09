0b7ac3b3e011c0f5009f8d3ab11f6ea71663b4ed
Moti_singh0007 <Moti.Singh@satin.local>
2024-06-10 01:02:52 +0530

    fibonaccinumber and square root

---
+++ dsapractise/src/dsa/basic/SquareRoot.java
@@ -0,0 +1,12 @@
+package dsa.basic;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double n=sc.nextInt();
+        System.out.println(Math.floor(Math.sqrt(n)));
+
+    }
+}
