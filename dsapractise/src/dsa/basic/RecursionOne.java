1cfae929d6aaa31ddb283d3d4f2ce538125e7506
Moti_singh0007 <Moti.Singh@satin.local>
2024-06-12 14:04:01 +0530

    recursion  problem.

---
+++ dsapractise/src/dsa/basic/RecursionOne.java
@@ -0,0 +1,19 @@
+package dsa.basic;
+
+import java.util.Scanner;
+
+public class RecursionOne {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+       int n= sc.nextInt();
+        System.out.println(factorial(n));
+    }
+    public static int factorial(int n){
+        if (n==1){
+            return 1;
+        }else {
+            int facto=factorial(n-1);
+            return facto*n;
+        }
+    }
+}
